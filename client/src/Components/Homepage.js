37588bfc5671555402b47826b89cc9dd8be8e9d3
c73lam8797 <c73lam@edu.uwaterloo.ca>
2020-03-07 23:21:04 -0500

    initial commit

---
+++ client/src/Components/Homepage.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Homepage extends Component {
+    constructor (props){
+        super (props);
+        this.state = {
+            createProfile: false,
+            login: false,
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => ({stressed:!state.stressed}));
+    }
+    
+    render() {
+        return (
+            <div >
+                <button onClick={this.handleClick} type = "submit">{this.state.stressed? 'Not Stressed':'Stressed'}?</button>
+
+                <Typography variant="h6" gutterBottom>
+                    <p style={{fontFamily: 'Montserrat', color: 'white'}}> You are currently... </p>
+                </Typography>
+                <h3 className="stressed" style={{fontFamily: 'Montserrat', backgroundColor: this.state.stressed? 'rgba(240,91,28,0.3)':'rgba(116, 169, 255, 0.3)'}}>{this.state.stressed? 'Stressed':'Not Stressed'}</h3>
+            </div>
+        )
+    }
+};
+
+export default Homepage;
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const apiPort = 5000
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+var config = require('./config');
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = config.firebaseConfig
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initilize Cloud Firestore
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+//initialize admin SDK using serciceAcountKey
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('yes.');
+    console.log("no.");
+
+    db.collection("cities").doc("asdf").set({
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+})
+
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+
+  });
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+
+
